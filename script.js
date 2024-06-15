58d10a494ac6ad60e9b834cc43311e1b6993999c
lalit-machra <lalitmachra0403@gmail.com>
2024-06-15 16:27:59 +0530

    Complete functionality of custom grid size

--- script.js
+++ script.js
@@ -1,23 +1,52 @@
 console.log("hello");
 
 const CONTAINER_SIZE = 790;
-const numOfGrids = 16;
+let numOfGrids = 16;
+
+const btn = document.querySelector("button");
+btn.addEventListener("click", () => {
+  do {
+    numOfGrids = +prompt("Enter the size of new grid (less than 100): ");
+  } while (numOfGrids > 100);
+
+  // Handling undefined values
+  if (!numOfGrids) {
+    numOfGrids = 16;
+  }
+
+  createNewGrid(numOfGrids);
+});
 
 const container = document.querySelector(".container");
+createNewGrid(numOfGrids);
 
-// A loop for laying numOfGrids rows
-for (let i = 0; i < numOfGrids; i++) {
-  let row = document.createElement("div");
-  row.classList.add("row");
-  container.appendChild(row);
+function createNewGrid(numOfGrids) {
+  // Making sure container is empty before new grid formation
+  container.innerHTML = "";
+
+  // A loop for laying numOfGrids rows
+  for (let i = 0; i < numOfGrids; i++) {
+    let row = document.createElement("div");
+    row.classList.add("row");
+    container.appendChild(row);
+  }
+
+  // A loop for laying numOfGrids columns inside each row
+  const rows = document.querySelectorAll(".row");
+  rows.forEach((row) => {
+    for (let i = 0; i < numOfGrids; i++) {
+      let column = document.createElement("div");
+      column.classList.add("column");
+      row.appendChild(column);
+    }
+  });
+
+  return;
 }
 
-// A loop for laying numOfGrids columns inside each row
-const rows = document.querySelectorAll(".row");
-rows.forEach((row) => {
-  for (let i = 0; i < numOfGrids; i++) {
-    let column = document.createElement("div");
-    column.classList.add("column");
-    row.appendChild(column);
+// Add hovering effects
+container.addEventListener("mouseover", (e) => {
+  if (e.target.classList.value != "container") {    // We don't want to color entire container
+    e.target.classList.add("colorIt");
   }
 });
